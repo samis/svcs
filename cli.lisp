25f8982e037a5bce6a0b65e113e9396a86707ac1
Samuel Hodgkins <samuel.hodgkins@sky.com>
2017-11-17 01:14:41 +0000

    Update, including a repository module

--- cli.lisp
+++ cli.lisp
@@ -1,9 +1,14 @@
-; Command-line interface for svcs
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+;;Command-line interface for svcs
+(import repo)
 (define subcommand (car arg))
 (defun print-version () ( print! "svcs version 0.0"))
 (case subcommand
       ["commit" (print! "commit")]
-      ["init" (print! "init")]
+      ["init" (repo/init)]
       ["checkout" (print! "checkout")]
       ["cat" (print! "cat")]
       ["status" (print! "status")]
